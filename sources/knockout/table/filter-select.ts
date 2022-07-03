c10f0ed261911575021917147f53191abefa627e
stabris <stabris@noreply.github.com>
2022-07-03 15:21:03 +0300

    Got rid of knockout in filter select dropdown

--- sources/knockout/table/filter-select.ts
+++ sources/knockout/table/filter-select.ts
@@ -8,7 +8,7 @@ ko.components.register("abris-filter-select", {
     viewModel: {
         createViewModel: function(params, componentInfo) {
             const viewModel = new TableFilterSelect(params.value, params.columnName, params.getColumnData, params.isOpen, params.title, params.moreText);
-            const close = () => viewModel.isOpen(false);
+            const close = () => viewModel.isOpen = false;
             document.body.addEventListener("click", close);
             ko.utils.domNodeDisposal.addDisposeCallback(componentInfo.element, function() {
                 document.body.removeEventListener("click", close);
