6db78bb73eb919c56c719971b1051b09a2d206e9
stabris <stabris@noreply.github.com>
2022-07-19 20:31:03 +0300

    Implemented Action class

--- sources/core/action.ts
+++ sources/core/action.ts
@@ -1,3 +1,6 @@
+import { Base } from "./base";
+import { property } from "./property";
+
 export interface IAction {
     name: string;
     action: () => void;
@@ -11,4 +14,23 @@ export interface IAction {
     cssClasses?: string;
     cssImage?: string;
     cssLabel?: string;
+}
+
+export class Action extends Base implements IAction {
+    constructor(source?: IAction) {
+        super();
+        Object.keys(source || {}).forEach(key => this[key] = source[key]);
+    }
+    @property() name: string;
+    @property() action: () => void;
+    @property() title: string;
+    @property() visible: boolean;
+    @property() enabled: boolean;
+    @property() active: boolean;
+    @property() icon: string;
+    @property() svg: string;
+    @property() container: string;
+    @property() cssClasses: string;
+    @property() cssImage: string;
+    @property() cssLabel: string;
 }
