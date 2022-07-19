50d02d1aef5f9bea20fc722fa4c7add176d8f8ca
stabris <stabris@noreply.github.com>
2022-07-19 20:32:52 +0300

    Make component reactive - WRONG - RE-WORK

--- sources/react/table/preheader.tsx
+++ sources/react/table/preheader.tsx
@@ -1,16 +1,19 @@
 import * as React from "react";
 import { TableWidget } from "../../table";
 import { SearchModel } from "../../table/search";
+import { makeReactive } from "../reactivity";
 import { AbrisComponentsTableSearch } from "./search";
 export interface IAbrisComponentsTableProps {
     model: TableWidget;
 }
 export function AbrisComponentsTablePreheader (props: IAbrisComponentsTableProps) {
-    
+    const searchModel = props.model.searchModel as SearchModel;
+    makeReactive(searchModel);
+
     return (
         <div className="abris-table-preheader">
             <div className="abris-table-search-group">
-                <AbrisComponentsTableSearch model={props.model.searchModel}></AbrisComponentsTableSearch>
+                <AbrisComponentsTableSearch model={searchModel}></AbrisComponentsTableSearch>
             </div>
         </div>
     );
