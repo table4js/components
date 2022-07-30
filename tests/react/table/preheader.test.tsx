d43f525534caf90a6791058a1f5cefc9eb5eeb2d
stabris <stabris@noreply.github.com>
2022-07-30 20:07:11 +0300

    Fixed snapshot test

--- tests/react/table/preheader.test.tsx
+++ tests/react/table/preheader.test.tsx
@@ -1,11 +1,11 @@
-import { fireEvent, render, screen} from '@testing-library/react'
 import React = require('react');
+import { fireEvent, render, screen} from '@testing-library/react'
 
 import { AbrisComponentsTablePreheader } from '../../../sources/react/table/preheader';
 import { ITableConfig, TableWidget } from '../../../sources/table';
 
 jest.mock("../../../sources/react/table/search", () => ({
-  AbrisComponentsTableSearch: (props) => {
+  AbrisSearch: (props) => {
     return "mock-table-search";
   }
 }));
