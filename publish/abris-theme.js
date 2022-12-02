117c941d05cf264bc2aba2960e2d8316d60777b6
SweetKatrin <kate.yagu@gmail.com>
2022-12-02 15:51:16 +0300

    fix doc

--- publish/abris-theme.js
+++ publish/abris-theme.js
@@ -21,31 +21,25 @@ function header() {
   }, /*#__PURE__*/ typedoc.JSX.createElement("a", {
     class: "header__link-page",
     href: "/"
-  }, "Main"), /*#__PURE__*/ typedoc.JSX.createElement("a", {
-    class: "header__link-page",
-    href: "/features"
-  }, "Features"), /*#__PURE__*/ typedoc.JSX.createElement("a", {
+  }, "Main"),  /*#__PURE__*/ typedoc.JSX.createElement("a", {
     class: "header__link-page",
     href: "/examples"
-  }, "Examples"), /*#__PURE__*/ typedoc.JSX.createElement("a", {
-    class: "header__link-page",
-    href: "/buy"
-  }, "Get support"), /*#__PURE__*/ typedoc.JSX.createElement("span", {
+  }, "Examples"), /*#__PURE__*/ typedoc.JSX.createElement("span", {
     class: "header__link-page header__link-page--active"
   }, "Documentation"), /*#__PURE__*/ typedoc.JSX.createElement("a", {
     class: "header__link-page",
     href: "#contact-form"
-  }, "Contact us"), /*#__PURE__*/ typedoc.JSX.createElement("a", {
-    class: "header__link-page",
-    href: "https://github.com/abris-platform/abris-components"
-  }, "GitHub")))));
+  }, "Contact us"), /*#__PURE__*/ typedoc.JSX.createElement("img", {
+    class: "social__icon",
+    src: "/img/footer/github.svg"
+  })))));
 }
+
 
 function footer() {
   return   /*#__PURE__*/ typedoc.JSX.createElement("section", {
-    class: "footer__docs"
   }, /*#__PURE__*/ typedoc.JSX.createElement("section", {
-    class: "form-back"
+    class: "section-container"
   },  /*#__PURE__*/ typedoc.JSX.createElement("div", {
     class: "page-size"
   }, /*#__PURE__*/ typedoc.JSX.createElement("div", {
@@ -53,61 +47,62 @@ function footer() {
     id: "contact-form"
   }, /*#__PURE__*/ typedoc.JSX.createElement("div", {
     class: "headline"
-  },  /*#__PURE__*/ typedoc.JSX.createElement("span", {
-    class: "headline__text" 
+  },  /*#__PURE__*/ typedoc.JSX.createElement("h2", {
+    class: "title-h2",
+    style: "text-align: center !important;" 
   },"Do you still have questions?")), /*#__PURE__*/ typedoc.JSX.createElement("form", {
     class: "form-back__form three-column",
     id: "contactForm",
     role: "form"
   }, /*#__PURE__*/ typedoc.JSX.createElement("span", {
-    class: "form-back__label" 
+    class: "text" 
   },"Name"), /*#__PURE__*/ typedoc.JSX.createElement("input", {
-    class: "form-back__field",
+    class: "form-back__field text",
     id: "name",
     type: "text",
     placeholder: "Name",
     required: "true"
   },), /*#__PURE__*/ typedoc.JSX.createElement("span", {
-    class: "form-back__label" 
+    class: "text" 
   },"E-mail"), /*#__PURE__*/ typedoc.JSX.createElement("input", {
-    class: "form-back__field",
+    class: "form-back__field text",
     id: "email",
     type: "email",
     placeholder: "E-mail",
     required: "true"
   },),/*#__PURE__*/ typedoc.JSX.createElement("span", {
-    class: "form-back__label" 
+    class: "text" 
   },"Message"),/*#__PURE__*/ typedoc.JSX.createElement("textarea", {
-    class: "form-back__field",
+    class: "form-back__field text",
     id: "message",
     rows: "5",
     type: "text",
     placeholder: "Enter your message..",
     required: "true"
   },),/*#__PURE__*/ typedoc.JSX.createElement("button", {
-    class: "button button--accent form-back__button",
+    class: "button button--accent form-back__button one-column",
     id: "form-submit",
     onclick: "formSuccess()",
     type: "button"
   },"Send"),)), /*#__PURE__*/ typedoc.JSX.createElement("div", {
-    class: "features__reason reason-left form-back__succes form-back__succes--hidden",
+    class: "h-card form-back__succes form-back__succes--hidden",
     id: "contact-succes"
   }, /*#__PURE__*/ typedoc.JSX.createElement("img", {
-    class: "reason-img",
+    class: "h-card__img two-column",
     src: "/img/form-back/1.svg"
   },),/*#__PURE__*/ typedoc.JSX.createElement("div", {
-    class: "reason-column"
+    class: "v-card two-column"
   }, /*#__PURE__*/ typedoc.JSX.createElement("span", {
-    class: "headline__text three-column" 
+    class: "title-h3" 
   },"Your message has been sent successfully!"),/*#__PURE__*/ typedoc.JSX.createElement("span", {
-    class: "reason__description two-column" 
+    class: "text" 
   },"We will contact you soon"),/*#__PURE__*/ typedoc.JSX.createElement("button", {
-    class: "button form-back__button",
+    class: "button button--accent",
     id: "new-letter",
     onclick: "newletter()",
     type: "button"
   },"Send a new message"))))),  /*#__PURE__*/ typedoc.JSX.createElement("section", {
-    class: "footer blue-top-background"
+    class: "section-container color-middle-background"
   }, /*#__PURE__*/ typedoc.JSX.createElement("div", {
     class: "page-size"
   }, /*#__PURE__*/ typedoc.JSX.createElement("div", {
