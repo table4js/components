2a4fb6aaab2071f767366726eba0adc0ca191177
tsv2013 <tsv2013@users.noreply.github.com>
2023-03-26 16:27:37 +0300

    Fixed #19 - Rows are not reordered if column sorting is reset to "none"

--- sources/utils/array-data-provider.ts
+++ sources/utils/array-data-provider.ts
@@ -2,11 +2,26 @@ import { IFilterItem } from "../core/find";
 import { IDataProvider } from "./data-provider";
 
 export class ArrayDataProvider implements IDataProvider {
-    constructor(public data: Array<any>) {
+    constructor(private _data: Array<any>) {
     }
 
-    filtered(filters, data) {
-        return data?.filter(row => 
+    ordered(order: Array<any>, data: Array<any> = []): Array<any> {
+        function sortfunc(a, b) {
+            for (let i = 0; i < order.length; i++) {
+                if (a[order[i].field] === b[order[i].field]) continue;
+                return (a[order[i].field] > b[order[i].field] ? 1 : -1) * (order[i].desc ? -1 : 1);
+            }
+            return 0;
+        }
+        const ordered = [].concat(data);
+        if(order.length > 0) {
+            ordered.sort(sortfunc);
+        }
+        return ordered;
+    }
+
+    filtered(filters: Array<IFilterItem>, data: Array<any> = []): Array<any> {
+        return data.filter(row => 
             filters.every(f => {
                 switch(f.op) {
                     case "EQ": return f.value.includes(row[f.field]); 
@@ -20,31 +35,17 @@ export class ArrayDataProvider implements IDataProvider {
     }
 
     getData(limit, offset, order, key, back, callback) {
-        function sortfunc(a, b) {
-            for (let i = 0; i < order.length; i++) {
-                if (a[order[i].field] === b[order[i].field]) continue;
-                return (a[order[i].field] > b[order[i].field] ? 1 : -1) * (order[i].desc ? -1 : 1);
-            }
-            return 0;
-        }
-        (order.length > 0) && this.data.sort(sortfunc);
-        let result = [];
-        const filteredData = this.filtered(this.filter, this.data);
-        for (var i = offset > 0 ? offset : 0; i < offset + limit && filteredData && i < filteredData.length; i++) {
-            result.push(filteredData[i]);
-        }
-        // setTimeout(function(){
-        //     console.log('2-секундная задержка прошла');
-        //     callback(result, offset + limit, filteredData.length, back)
-        // }, 2000);
-        callback(result, offset + limit, filteredData.length, back);
+        let result = this.ordered(order, this._data);
+        result = this.filtered(this.filter, result);
+        result = result.slice(offset, limit);
+        callback(result, offset + limit, result.length, back);
     }
 
     getSummary(func, field, callback) {
-        const filteredData = this.filtered(this.filter, this.data);
+        const filteredData = this.filtered(this.filter, this._data);
         let result = filteredData.length ? filteredData[0][field] : false;
         let sum = 0, count = 0, uniques = [];
-        this.data.forEach(row => {
+        this._data.forEach(row => {
             switch (func) {
                 case "sum": result = result + row[field]; break;
                 case "avg": sum = sum + row[field]; count++; result = sum / count; break;
@@ -60,7 +61,7 @@ export class ArrayDataProvider implements IDataProvider {
 
     getColumnData(columnName, filter, limit, offset, callback) {
         let result = [], uniques = [];
-        const filteredData = this.data.map(row => {
+        const filteredData = this._data.map(row => {
             if ((!(filter) || ~row[columnName].toUpperCase().indexOf(filter.toUpperCase())) && !uniques.includes(row[columnName])) { uniques.push(row[columnName]); };
         });
         for (var i = offset > 0 ? offset : 0; i < offset + limit && uniques && i < uniques.length; i++) {
@@ -70,20 +71,20 @@ export class ArrayDataProvider implements IDataProvider {
     }
 
     saveData(keyName:string, key:any, modify:{}) {
-        Object.keys(modify).forEach( p=> this.data.find(r => r[keyName] == key)[p] = modify[p]);
+        Object.keys(modify).forEach(p => this._data.find(r => r[keyName] == key)[p] = modify[p]);
         return true;
     }
 
     insertData(keyName:string, modify:{}) {
         console.log(modify);
-        modify[keyName] = this.data.length + 1;
-        this.data.push(modify);
+        modify[keyName] = this._data.length + 1;
+        this._data.push(modify);
         return true;
     }
 
     deleteData(keyName:string, keys:any[], callback) {
-        keys.forEach(k => this.data.find(r => this.data.splice(this.data.indexOf(this.data.find(r => r[keyName] == k)),1)))
-        console.log(this.data);
+        keys.forEach(k => this._data.find(r => this._data.splice(this._data.indexOf(this._data.find(r => r[keyName] == k)),1)))
+        console.log(this._data);
         callback(true);
     } 
 
