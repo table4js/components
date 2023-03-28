e18141059e178d547026bbcf1bdeab08b334bf3e
tsv2013 <tsv2013@users.noreply.github.com>
2023-03-28 07:52:41 +0300

    Added filter and summary f-tests

--- e2e/table-basic.spec.ts
+++ e2e/table-basic.spec.ts
@@ -92,11 +92,14 @@ test.describe('Basic functionality', () => {
   });
 
   test('search', async ({ page }) => {
+    const rows = await page.locator('.table4js__body tr');
     await expect(page.getByText('Total: 1000')).toBeVisible();
     await page.getByPlaceholder('Search...').click();
     await page.getByPlaceholder('Search...').fill('Adams');
     await page.getByPlaceholder('Search...').press('Enter');
     await expect(page.getByText('Total: 11')).toBeVisible();
+    await expect(rows).toHaveCount(11);
+
     await page.getByPlaceholder('Search...').click();
     await page.getByPlaceholder('Search...').fill('');
     await page.getByPlaceholder('Search...').press('Enter');
@@ -104,10 +107,48 @@ test.describe('Basic functionality', () => {
   });
 
   test('filter by column', async ({ page }) => {
+    const rows = await page.locator('.table4js__body tr');
+    const col1Header = await page.getByRole('cell', { name: 'First Name' });
     const col2Header = await page.getByRole('cell', { name: 'Last Name' });
     await col2Header.hover();
-    const columnFilter = await col2Header.locator('.table4js-title__filter');
-    await columnFilter.click();
-    
+    const column2Filter = await col2Header.locator('.table4js-title__filter');
+    await column2Filter.click();
+    await expect(page.locator('.table4js-filter__content')).toHaveCount(1);
+    await page.getByPlaceholder('*').click();
+    await page.getByPlaceholder('*').fill('Adams');
+    await page.getByPlaceholder('*').press('Enter');
+    await expect(page.getByText('Total: 11')).toBeVisible();
+    await expect(rows).toHaveCount(11);
+
+    await col1Header.hover();
+    const column1Filter = await col1Header.locator('.table4js-title__filter');
+    await column1Filter.click();
+    await expect(page.locator('.table4js-filter__content')).toHaveCount(2);
+    await page.getByPlaceholder('*').first().click();
+    await page.getByPlaceholder('*').first().fill('Henry');
+    await page.getByPlaceholder('*').first().press('Enter');
+    await expect(page.getByText('Total: 1')).toBeVisible();
+    await expect(rows).toHaveCount(1);
+
+    await page.locator('.table4js-filter__content').nth(1).locator('.table4js-filter__remove-icon').click();
+    await expect(page.locator('.table4js-filter__content')).toHaveCount(1);
+    await expect(page.getByText('Total: 4')).toBeVisible();
+    await expect(rows).toHaveCount(4);
+
+    await page.locator('.table4js-filter__content').nth(0).locator('.table4js-filter__remove-icon').click();
+    await expect(page.locator('.table4js-filter__content')).toBeHidden();
+    await expect(page.getByText('Total: 1000')).toBeVisible();
+  });
+
+  test('summary', async ({ page }) => {
+    await expect(page.getByRole('row').filter({ hasText: '-NU-NU-Σx̄minmaxNU-minmaxNU-NU-Σx̄minmaxNU' }).locator('svg')).toBeHidden();
+    await page.getByRole('button', { name: 'Summary' }).click();
+    await expect(page.getByRole('row').filter({ hasText: '-NU-NU-Σx̄minmaxNU-minmaxNU-NU-Σx̄minmaxNU' }).locator('svg')).toBeVisible();
+
+    await page.locator('.table4js-summary__select').nth(0).selectOption('count');
+    await expect(page.getByRole('cell', { name: '1000' }).filter({ hasText: '1000-NU' })).toBeVisible();
+
+    await page.locator('.table4js-summary__select').nth(2).selectOption('avg');
+    await expect(page.getByText('-71.159')).toBeVisible();
   });
 });
